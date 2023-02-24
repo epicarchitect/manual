7d653a163c35de317685f11b50b28ce5ae5b0564
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-02-24 16:46:10 +0700

    up versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     /** Android Framework */
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.work:work-runtime-ktx:2.7.1")
-    implementation("androidx.fragment:fragment-ktx:1.4.1")
+    implementation("androidx.work:work-runtime-ktx:2.8.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.4.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
