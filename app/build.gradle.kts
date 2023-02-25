57171101e7f882e4a6ea1b94493a41c0f50e5d98
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-02-25 15:49:27 +0700

    migrate to ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9"
 }
 
 android {
@@ -47,7 +47,7 @@ dependencies {
     /** Android Framework */
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("androidx.work:work-runtime-ktx:2.8.0")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.5.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
@@ -71,7 +71,7 @@ dependencies {
     /** Images */
     implementation("com.github.stfalcon-studio:StfalconImageViewer:v1.0.1")
     implementation("com.github.bumptech.glide:glide:4.14.2")
-    kapt("com.github.bumptech.glide:compiler:4.14.2")
+    ksp("com.github.bumptech.glide:compiler:4.14.2")
 
     /** Video */
     implementation("com.google.android.exoplayer:exoplayer:2.18.3")
@@ -80,9 +80,9 @@ dependencies {
     implementation("me.saket:better-link-movement-method:2.2.0")
 
     /** Database */
-    implementation("androidx.room:room-runtime:2.4.2")
-    implementation("androidx.room:room-ktx:2.4.2")
-    kapt("androidx.room:room-compiler:2.4.2")
+    implementation("androidx.room:room-runtime:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
+    ksp("androidx.room:room-compiler:2.5.0")
 
     /** Ads */
     implementation("com.google.android.gms:play-services-ads:21.5.0")
