c935a8cea739622bb99cda9ceb94ac8481d41d5b
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-05-15 03:47:27 +0700

    fix GDPRHelper

--- app/src/main/java/manual/app/ads/GDPRHelper.kt
+++ app/src/main/java/manual/app/ads/GDPRHelper.kt
@@ -13,62 +13,66 @@ import java.net.URL
 
 class GDPRHelper(private val context: Context) {
 
-    private var form: ConsentForm? = null
-    private val consentInformation = ConsentInformation.getInstance(context)
+//    private var form: ConsentForm? = null
+//    private val consentInformation = ConsentInformation.getInstance(context)
 
     val isEEA: Boolean
-        get() = consentInformation.isRequestLocationInEeaOrUnknown
+//        get() = consentInformation.isRequestLocationInEeaOrUnknown
+        get() = false
 
     val consentStatus: ConsentStatus
-        get() = consentInformation.consentStatus
+        get() = ConsentStatus.UNKNOWN
+//        get() = consentInformation.consentStatus
 
     fun checkConsent(activity: Activity, onStatusChangeListener: OnStatusChangeListener) {
-        val publisherIds = arrayOf(context.getString(R.string.admob_publisher_id))
-        consentInformation.requestConsentInfoUpdate(
-            publisherIds,
-            object : ConsentInfoUpdateListener {
-                override fun onConsentInfoUpdated(status: ConsentStatus) {
-                    if (isEEA) {
-                        if (status === ConsentStatus.UNKNOWN) {
-                            openConsentDialog(activity, onStatusChangeListener)
-                        } else {
-                            onStatusChangeListener.onChange(status)
-                        }
-                    } else {
-                        onStatusChangeListener.onChange(status)
-                    }
-                }
-
-                override fun onFailedToUpdateConsentInfo(errorDescription: String) {
-                    Toast.makeText(activity, errorDescription, Toast.LENGTH_SHORT).show()
-                }
-            })
+        // because deprecated
+        onStatusChangeListener.onChange(ConsentStatus.UNKNOWN)
+//        val publisherIds = arrayOf(context.getString(R.string.admob_publisher_id))
+//        consentInformation.requestConsentInfoUpdate(
+//            publisherIds,
+//            object : ConsentInfoUpdateListener {
+//                override fun onConsentInfoUpdated(status: ConsentStatus) {
+//                    if (isEEA) {
+//                        if (status === ConsentStatus.UNKNOWN) {
+//                            openConsentDialog(activity, onStatusChangeListener)
+//                        } else {
+//                            onStatusChangeListener.onChange(status)
+//                        }
+//                    } else {
+//                        onStatusChangeListener.onChange(status)
+//                    }
+//                }
+//
+//                override fun onFailedToUpdateConsentInfo(errorDescription: String) {
+//                    Toast.makeText(activity, errorDescription, Toast.LENGTH_SHORT).show()
+//                }
+//            })
     }
-
-    fun openConsentDialog(activity: Activity, onStatusChangeListener: OnStatusChangeListener) {
-        try {
-            form = ConsentForm.Builder(activity, URL(context.getString(R.string.privacyPolicy_url)))
-                .withListener(object : ConsentFormListener() {
-                    override fun onConsentFormLoaded() {
-                        form!!.show()
-                    }
-
-                    override fun onConsentFormClosed(
-                        status: ConsentStatus?,
-                        userPrefersAdFree: Boolean?
-                    ) {
-                        onStatusChangeListener.onChange(status)
-                    }
-                })
-                .withPersonalizedAdsOption()
-                .withNonPersonalizedAdsOption()
-                .build()
-
-            form!!.load()
-        } catch (t: Throwable) {
-            t.printStackTrace()
-        }
-    }
+//
+//    fun openConsentDialog(activity: Activity, onStatusChangeListener: OnStatusChangeListener) {
+//        try {
+//            form = ConsentForm.Builder(activity, URL(context.getString(R.string.privacyPolicy_url)))
+//                .withListener(object : ConsentFormListener() {
+//                    override fun onConsentFormLoaded() {
+//                        form!!.show()
+//                    }
+//
+//                    override fun onConsentFormClosed(
+//                        status: ConsentStatus?,
+//                        userPrefersAdFree: Boolean?
+//                    ) {
+//                        onStatusChangeListener.onChange(status)
+//                    }
+//                })
+//                .withPersonalizedAdsOption()
+//                .withNonPersonalizedAdsOption()
+//                .build()
+//
+//            form!!.load()
+//        } catch (t: Throwable) {
+//            t.printStackTrace()
+//        }
+//    }
 
     fun interface OnStatusChangeListener {
         fun onChange(status: ConsentStatus?)
