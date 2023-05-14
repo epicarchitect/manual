c08fcabbcc4063db096bb67b6e9aedeef039237f
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-05-15 06:09:03 +0700

    up versions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
 dependencies {
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.work:work-runtime-ktx:2.8.0")
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.5.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
